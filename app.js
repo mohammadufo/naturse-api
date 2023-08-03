99e7510213c5c37c880aba8c60307862559c928c
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-08-04 01:16:21 +0330

    add tour route

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
@@ -14,6 +16,83 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours,
     },
+  });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+
+  const tour = tours.find((el) => el.id === +id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'tour not found!',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: newId }, req.body);
+  const newTour = { ...req.body, id: newId };
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+
+  // res.status(201).send('Done!');
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+
+  const tour = tours.find((tour) => tour.id === +id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid id',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'updated tour will be here',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+
+  const tour = tours.find((tour) => tour.id === +id);
+
+  if (!tour)
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid id',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
   });
 });
 
