1f5d57a418f44dfc29b84eb6dfed763e82fdd2d5
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-08-04 14:26:02 +0330

    add some test middlewares

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const express = require('express');
 const fs = require('fs');
+
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware! 👋🏻');
+
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
     results: tours.length,
