b0bbb53559f97839ba8d3aacefa1baabb34e2cc0
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-08-23 20:18:01 +0330

    add 404 route and error handler middleware

--- app.js
+++ app.js
@@ -22,10 +22,21 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'failed',
-    message: `Cant find ${req.originalUrl} on this server!`,
+app.all('*', (req, res, next) => {
+  const err = new Error(`Cant find ${req.originalUrl} on this server!`);
+  err.status = 'failed';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
