c70614b01f5690acafaa20ad936a3592848ac3c6
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-09-29 17:51:45 +0330

    add index for some tour fields

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 dotenv.config({ path: './.env' });
 
@@ -18,10 +20,18 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync('./dev-data/data/tours.json', 'utf-8')
 );
+const users = JSON.parse(
+  fs.readFileSync('./dev-data/data/users.json', 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync('./dev-data/data/reviews.json', 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data Successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -32,6 +42,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Successfully deleted!');
   } catch (err) {
     console.log(err);
