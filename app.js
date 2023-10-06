f3368197cf1e32c815f55f98c3e960da2fd2db73
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-10-06 18:11:51 +0330

    add corse library

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cors = require('cors');
 
 const path = require('path');
 
@@ -17,6 +18,8 @@ const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
 
 app.use(express.json({ limit: '10kb' }));
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
